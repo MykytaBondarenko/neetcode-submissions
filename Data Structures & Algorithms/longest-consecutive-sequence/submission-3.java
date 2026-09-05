023df960617b56b4556f082b11c38006644c5cf1
Mykyta <153509818+MykytaBondarenko@users.noreply.github.com>
2026-09-05 16:59:04 +0100

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.java
@@ -0,0 +1,76 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        // // Approach 1
+        // if (nums.length == 0) {
+        //     return 0;
+        // }
+
+        // int min = nums[0];
+        // int max = nums[0];
+        // HashSet<Integer> set = new HashSet<>();
+
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (min > nums[i]) {
+        //         min = nums[i];
+        //     }
+        //     if (max < nums[i]) {
+        //         max = nums[i];
+        //     }
+        //     set.add(nums[i]);
+        // }
+
+        // int maxSeq = 0;
+        // int curSeq = 0;
+        // for (int i = min; i <= max; i++) {
+        //     if (set.contains(i)) {
+        //         curSeq++;
+        //     } else {
+        //         if (maxSeq < curSeq) {
+        //             maxSeq = curSeq;
+        //         }
+        //         curSeq = 0;
+        //     }
+        // }
+        // if (maxSeq < curSeq) {
+        //     maxSeq = curSeq;
+        // }
+        // return maxSeq;
+
+        // Approach 2
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int maxSequence = 0;
+        for (int num : nums) {
+            if (!set.contains(num - 1)) {
+                int curSequence = 1;
+                while (set.contains(num + curSequence)) {
+                    curSequence++;
+                }
+                maxSequence = Math.max(maxSequence, curSequence);
+            }
+        }
+        return maxSequence;
+    }
+}
+
+/*
+
+Approach 1: O(n + m) time, O(n) space (time limit exceded)
+Go through all of the elements in nums and:
+1. Find min and max elements
+2. Put all the elements in the HashSet
+Then, start going from min to max
+Check if num is in the hashset
+Track the sequences
+If the sequence stops, update maxsequence and reset
+
+Approach 2: O(n) time, O(n) space
+Put all the array elements into a HashSet
+A number can be a "start of a sequence" only if there is no num-1 in the set
+If we find a number like this, we start checking how many consequtive nums there are after that
+Update the maxSequence
+
+*/
